05c0d91432564501b0c28f596386e4ed53f31d0e
ebiharahiroki <laoyuanhonggui@gmail.com>
2023-05-13 11:19:16 +0900

    forの後に半角スペースを追加

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 import java.util.ArrayList;
 import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         List<String> names = new ArrayList<>();
         names.add("りんご");
         names.add("みかん");
         names.add("ぶどう");
-        for(String name : names) {
+        for (String name : names) {
             if (name.equals("みかん"))
                 System.out.println("私の好きな食べ物は" + name + "です");
         }
